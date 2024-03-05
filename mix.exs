cb8474ad1d9525c63a24d4e4e31382e80caaef60
wldmr <nope@wldmr.net>
2024-03-05 17:04:38 +0100

    Add :observer extra application

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule InaraBotEx.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :wx, :runtime_tools, :observer],
       mod: {InaraBot.Application, []}
     ]
   end
