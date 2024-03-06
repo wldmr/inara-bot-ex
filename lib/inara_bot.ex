85832e754a7b829a779c7c75917c538cb1f5853f
wldmr <nope@wldmr.net>
2024-03-06 16:14:38 +0100

    Include Reddit articles when fetching new posts

--- lib/inara_bot.ex
+++ lib/inara_bot.ex
@@ -6,7 +6,8 @@ defmodule InaraBot do
 
   @re ~r/the serenity\b(?!\s+crew|cast|movie|series)/i
 
-  @forum "firefly"
+  # @forum "firefly"
+  @forum "wldmr_bot_practice"
 
   @opaque t() :: String.t() | nil
 
@@ -19,18 +20,32 @@ defmodule InaraBot do
 
   ## Examples
 
-      iex> InaraBot.respond_to(%Post{body: "I like the Serenity very much"})
+      iex> InaraBot.respond_to(%Post{body: "I like the Serenity."})
       %Post{body: "> the Serenity\\n\\nIt's just Serenity."}
 
       iex> InaraBot.respond_to(%Post{body: "I like the serenity very much"})
       %Post{body: "> the serenity\\n\\nIt's just Serenity."}
 
+  Only the first occurrence is admonished:
+
+      iex> InaraBot.respond_to(%Post{body: "I like the serenity very much"})
+      %Post{body: "> the serenity\\n\\nIt's just Serenity."}
+
+  including the heading, if present:
+
+      iex> InaraBot.respond_to(%Post{heading: "The Serenity is cool", body: "I like the serenity very much"})
+      %Post{body: "> The Serenity\\n\\nIt's just Serenity."}
+
+  There are exceptions where the construction uses "serenity" as an adjective:
+
       iex> InaraBot.respond_to(%Post{body: "I like the Serenity crew very much"})
       nil
+
   """
   @spec respond_to(Post.t()) :: Post.t() | nil
   def respond_to(msg) do
-    case Regex.run(@re, msg.body) do
+    text = "#{msg.heading || ""}\n\n#{msg.body || ""}"
+    case Regex.run(@re, text) do
       [offender] ->
         %Post{
           body: "> #{offender}\n\nIt's just Serenity.",
@@ -44,7 +59,7 @@ defmodule InaraBot do
 
   @spec check_and_respond(atom(), String.t()) :: t()
   def check_and_respond(identity, last_seen_post) do
-    new_posts = Reddit.fetch_latest(identity, @forum, last_seen_post)
+    {new_posts, token} = Reddit.fetch_latest(identity, @forum, last_seen_post)
 
     Enum.each(new_posts, &Logger.debug("New Post: " <> inspect(&1)))
 
@@ -53,12 +68,7 @@ defmodule InaraBot do
     |> Enum.reject(&is_nil/1)
     |> Enum.each(fn post -> Reddit.send_post(identity, post) end)
 
-    new_last_seen_post =
-      new_posts
-      |> Enum.max_by(& &1.timestamp, fn -> %{} end)
-      |> Map.get(:id, last_seen_post)
-
-    Logger.debug("Latest: #{last_seen_post || "∅"} → #{new_last_seen_post}")
-    new_last_seen_post
+    Logger.debug("Latest: #{inspect(last_seen_post)} → #{inspect(token)}")
+    token
   end
 end
--- lib/reddit.ex
+++ lib/reddit.ex
@@ -2,37 +2,23 @@ defmodule Reddit do
   require Logger
   use Util.Sections
 
+  @opaque latest_token() :: %{comment: Post.id(), article: Post.id()}
+
+  @comment_kind "t1_"
+  @article_kind "t3_"
+
+  @spec fetch_latest(atom(), String.t(), latest_token()) :: {list(Post.t()), latest_token()}
   def fetch_latest(identity, subreddit, latest_so_far \\ nil) do
-    uri = URI.new!("/r/#{subreddit}/comments")
+    latest_comment = if latest_so_far, do: Map.get(latest_so_far, :comment), else: nil
+    latest_article = if latest_so_far, do: Map.get(latest_so_far, :article), else: nil
 
-    uri =
-      if latest_so_far,
-        do: URI.append_query(uri, URI.encode_query(before: latest_so_far)),
-        else: uri
+    {comments, comment_token} = latest_comments(identity, subreddit, latest_comment)
+    {articles, article_token} = latest_articles(identity, subreddit, latest_article)
 
-    response = Reddit.Auth.get!(identity, uri)
+    posts = articles ++ comments
+    token = %{comment: comment_token, article: article_token}
 
-    defsection :ignored, "Inspect Comment fields" do
-      Logger.debug(
-        "Comment fields: " <>
-          (Enum.flat_map(response.body["data"]["children"], &Map.keys(&1["data"]))
-           |> Enum.uniq()
-           |> Enum.join(", "))
-      )
-    end
-
-    response.body["data"]["children"]
-    |> Enum.map(&{&1["kind"], &1["data"]})
-    |> Enum.map(fn {kind, post} ->
-      %Post{
-        id: "#{kind}_#{post["id"]}",
-        username: post["author"],
-        parent: post["parent_id"],
-        heading: nil,
-        body: post["body"],
-        timestamp: post["created"] |> trunc() |> DateTime.from_unix!()
-      }
-    end)
+    {posts, token}
   end
 
   def send_post(identity, post) do
@@ -46,5 +32,87 @@ defmodule Reddit do
   @spec start_link(keyword()) :: :ignore | {:error, any()} | {:ok, pid()}
   def start_link(identity) do
     Supervisor.start_link(__MODULE__, identity, name: String.to_atom("#{__MODULE__}.#{identity}"))
+  end
+
+  defp latest_comments(identity, subreddit, comment_token) do
+    uri = URI.new!("/r/#{subreddit}/comments")
+
+    uri =
+      if comment_token,
+        do: URI.append_query(uri, URI.encode_query(before: comment_token)),
+        else: uri
+
+    response = Reddit.Auth.get!(identity, uri)
+
+    defsection :ignored, "Inspect Comment fields" do
+      Logger.debug(
+        "Comment fields: " <>
+          (Enum.flat_map(response.body["data"]["children"], &Map.keys(&1["data"]))
+           |> Enum.uniq()
+           |> Enum.join(", "))
+      )
+    end
+
+    comments =
+      response.body["data"]["children"]
+      |> Enum.map(& &1["data"])
+      |> Enum.map(fn post ->
+        %Post{
+          id: "#{@comment_kind}#{post["id"]}",
+          username: post["author"],
+          parent: post["parent_id"],
+          heading: nil,
+          body: post["body"],
+          timestamp: post["created"] |> trunc() |> DateTime.from_unix!()
+        }
+      end)
+
+    comment_token =
+      comments
+      |> Enum.max_by(& &1.timestamp, fn -> %{} end)
+      |> Map.get(:id, comment_token)
+
+    {comments, comment_token}
+  end
+
+  defp latest_articles(identity, subreddit, article_token) do
+    uri = URI.new!("/r/#{subreddit}/new")
+
+    uri =
+      if article_token,
+        do: URI.append_query(uri, URI.encode_query(before: article_token)),
+        else: uri
+
+    response = Reddit.Auth.get!(identity, uri)
+
+    defsection :ignored, "Inspect Article fields" do
+      Logger.debug(
+        "Article fields: " <>
+          (Enum.flat_map(response.body["data"]["children"], &Map.keys(&1["data"]))
+           |> Enum.uniq()
+           |> Enum.join(", "))
+      )
+    end
+
+    articles =
+      response.body["data"]["children"]
+      |> Enum.map(& &1["data"])
+      |> Enum.map(fn post ->
+        %Post{
+          id: "#{@article_kind}#{post["id"]}",
+          username: post["author"],
+          parent: post["parent_id"],
+          heading: post["title"],
+          body: post["selftext"],
+          timestamp: post["created"] |> trunc() |> DateTime.from_unix!()
+        }
+      end)
+
+    article_token =
+      articles
+      |> Enum.max_by(& &1.timestamp, fn -> %{} end)
+      |> Map.get(:id, article_token)
+
+    {articles, article_token}
   end
 end
